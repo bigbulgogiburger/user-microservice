72303c0365ab2dacd0765cc6e89acd0d7ac3c6f1
dhpyun <dhpyun@dbinc.co.kr>
2026-03-24 15:04:37 +0900

    chore: Gradle 8.3으로 업그레이드

--- src/main/java/com/example/usermicroservice/UserMicroserviceApplication.java
+++ src/main/java/com/example/usermicroservice/UserMicroserviceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -21,5 +23,6 @@ public class UserMicroserviceApplication {
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
 
 }
